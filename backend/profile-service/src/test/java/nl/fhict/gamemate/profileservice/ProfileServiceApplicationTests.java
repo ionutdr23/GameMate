b4e9e86ad06da654fd6e5ae869f70c0c96e619b2
Dragomir, Ionut C.I. <ionutjocuri2003@gmail.com>
2025-04-06 19:28:04 +0200

    Set up profile service for sonarqube check

--- backend/profile-service/src/test/java/nl/fhict/gamemate/profileservice/ProfileServiceApplicationTests.java
+++ backend/profile-service/src/test/java/nl/fhict/gamemate/profileservice/ProfileServiceApplicationTests.java
@@ -1,13 +1,25 @@
 package nl.fhict.gamemate.profileservice;
 
+import nl.fhict.gamemate.profileservice.repository.ProfileRepository;
+import nl.fhict.gamemate.profileservice.service.ProfileService;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-@SpringBootTest
-class ProfileServiceApplicationTests {
+@ExtendWith(MockitoExtension.class)
+class ProfileServiceTest {
+
+	@Mock
+	private ProfileRepository profileRepository;
+
+	@InjectMocks
+	private ProfileService profileService;
 
 	@Test
 	void contextLoads() {
-	}
 
+	}
 }
+
